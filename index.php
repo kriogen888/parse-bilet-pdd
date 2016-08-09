d4e8bdb9411c81efdce2c8ef57ff6a12c38b2e44
Igory L <kriogen888@gmail.com>
2016-08-10 05:12:09 +0700

    add questions array

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
 <?php
+//set_time_limit(100);
 const IMG_DIR = __DIR__ . "/bilet/img/";
 const TEXT_DIR = __DIR__ . "/bilet/txt/";
+const IN_CHARSET = "windows-1251";
+const OUT_CHARSET = "utf-8";
+$billet_arr = [];
 
+//Извлекаем из директории файлы
+foreach (getFile(TEXT_DIR) as $item) {
+    //Разбиваем файл на строки
+
+    $filename = $item->getBasename();
+
+    $bilet = (int)substr($filename, 0, 2);
+    $vopros = (int)substr($filename, 2, 2);
+    $key = ($bilet - 1) * 20 + $vopros;
+
+    $billet_arr[$key] = [
+        'bilet' => $bilet,
+        'vopros' => $vopros,
+        'correct_answer' => (int)$item->current(),
+
+    ];
+    $item->next();
+    $billet_arr[$key]['questions'] = clearLine($item->current());
+    $item->next();
+    $billet_arr[$key]['answers'] = '';
+    while ($item->valid()) {
+//        echo $item->current();
+        if (clearLine($item->current()) == '*') break;
+        $billet_arr[$key]['answers'] = $billet_arr[$key]['answers'] . clearLine($item->current()) . "|";
+        $item->next();
+    }
+    $item->next();
+    $billet_arr[$key]['comment'] = clearLine($item->current());
+}
+
+var_dump($billet_arr);
 
 function getFile($dir)
 {
@@ -9,18 +44,11 @@ function getFile($dir)
     foreach ($dir_iterator as $item) {
         //Проверяем, что это файл, а не точка
         if (!$dir_iterator->isFile()) continue;
-
         yield new SplFileObject($dir . $item);
-
-//        echo $dir_iterator->key() . " => $item <br>";
     }
 }
 
-//Разбиваем файл на строки
-foreach (getFile(TEXT_DIR) as $item) {
-
-    foreach ($item as $line) {
-        echo iconv('windows-1251', 'utf-8', $line) . "<br>";
-    }
-
+function clearLine($line)
+{
+    return iconv(IN_CHARSET, OUT_CHARSET, preg_replace('/\\r\\n?|\\n/', '', $line));
 }
